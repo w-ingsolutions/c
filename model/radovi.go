b960fa791aa7ef6b009a5df5798c154c63746f7a
marcetin <marcetin@gmail.com>
2020-06-28 20:32:50 +0200

    Drugi

---
+++ model/radovi.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"gioui.org/widget"
+)
+
+type WingVrstaRadova struct {
+	Id                 int                            `json:"id"`
+	Naziv              string                         `json:"naziv"`
+	Opis               string                         `json:"opis"`
+	Obracun            string                         `json:"obracun"`
+	Jedinica           string                         `json:"jedinica"`
+	Cena               float64                        `json:"cena"`
+	Slug               string                         `json:"slug"`
+	Omogucen           bool                           `json:"omogucen"`
+	Baza               bool                           `json:"baza"`
+	Element            bool                           `json:"element"`
+	PodvrsteRadova     map[int]WingVrstaRadova        `json:"podvrsteradova"`
+	NeophodanMaterijal map[int]WingNeophodanMaterijal `json:"neophodanmaterijal"`
+}
+
+type WingIzabraniElementi struct {
+	Id                             string
+	SumaCena                       float64
+	SumaCenaMaterijal              float64
+	Elementi                       []*WingIzabraniElement
+	ElementiPrikaz                 []*WingIzabraniElement
+	UkupanNeophodanMaterijal       map[int]WingNeophodanMaterijal
+	UkupanNeophodanMaterijalPrikaz map[int]WingNeophodanMaterijal
+}
+
+type WingIzabraniElement struct {
+	Sifra         string
+	Kolicina      int
+	SumaCena      float64
+	Element       WingVrstaRadova
+	DugmeBrisanje *widget.Clickable
+}
+
+type WingCalGrupaRadova struct {
+	Id       string                  `json:"id"`
+	Slug     string                  `json:"slug"`
+	Elementi map[int]WingVrstaRadova `json:"elementi"`
+}
