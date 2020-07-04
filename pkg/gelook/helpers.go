f7e4e5069ddcf1348d9b5a46e58dc24e0335d71b
marcetin <marcetin@gmail.com>
2020-07-05 00:37:09 +0200

    Drugi

--- pkg/gelook/helpers.go
+++ pkg/gelook/helpers.go
@@ -64,7 +64,7 @@ func (t *WingUItheme) WingUIline(vert bool, verticalPadding, horizontalPadding f
 			h := gtx.Constraints.Max.X
 			if vert {
 				h = size
-				v = 30
+				v = gtx.Constraints.Max.Y
 			}
 			return DuoUIdrawRectangle(gtx, h, v, color, [4]float32{0, 0, 0, 0}, [4]float32{0, 0, 0, 0})
 		})
